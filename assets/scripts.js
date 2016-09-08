48d0773c5ca563abf93774a6eebe7ee90388f797
HyperSprite <chris-github@hypersprite.com>
2016-09-08 14:02:20 -0700

    added wbr image, working on email form

--- assets/scripts.js
+++ assets/scripts.js
@@ -110,3 +110,27 @@ oReq.open('GET', jsonFile);
 oReq.send();
 })();
 
+// Modal
+var modal = document.getElementById('myModal');
+
+var emaillink = document.getElementById("open-email-modal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+emaillink.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
