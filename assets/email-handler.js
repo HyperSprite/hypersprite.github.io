efd85dfb971ead3baad7452167a509ab02d43d5d
Christopher Dunavan <github@hypersprite.com>
2016-09-19 22:42:42 -0700

    email form - added client side captcha

--- assets/email-handler.js
+++ assets/email-handler.js
@@ -1,13 +1,16 @@
-// Credit for this script goes to https://github.com/dwyl/html-form-send-email-via-google-script-without-server
+
+// _eG are the emailGlobals
+var _eG = {};
+_eG.canceled = false;
+_eG.iHateBots = '';
 
 // Email submit progress bar
-
 function statusMove(moveWhat) {
   var elem = document.getElementById(moveWhat);
   var width = 30;
   var id = setInterval(frame, 10);
   function frame() {
-    if (width >= 100) {
+    if (width >= 100 || _eG.canceled) {
       clearInterval(id);
     } else {
       width++;
@@ -15,6 +18,36 @@ function statusMove(moveWhat) {
     }
   }
 }
+
+function captcha(){
+  var alpha = ['A','B','C','D','E','F','G','H','J','K','M','N','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','j','k','m','n','p','q','r','s','t','u','v','w','x','y','z','2','3','4','5','6','7','8','9','-','=','+'];
+  _eG.iHateBots = '';
+  var i;
+  for (i = 0; i < 7; i++) {
+    _eG.iHateBots += alpha[Math.floor(Math.random() * alpha.length)];
+  }
+  var canvas = document.getElementById('email-canvas');
+  var context = canvas.getContext('2d');
+  context.clearRect(0, 0, 150, 50);
+  context.font = '20px courier';
+  context.fillText(_eG.iHateBots.split('').join(' '), -18, 30);
+}
+
+function validCaptcha() {
+  var checkThis = document.getElementById('ihatebots').value;
+  if (_eG.iHateBots.substring(1) === checkThis) {
+    return true;
+  }
+  return false;
+}
+
+var refreshCaptcha = document.getElementById('refresh');
+refreshCaptcha.onclick = function () {
+  captcha();
+  return false;
+};
+// Credit for most of the stuff below this line goes to:
+// https://github.com/dwyl/html-form-send-email-via-google-script-without-server
 
 
 function validEmail(email) {
@@ -34,40 +67,38 @@ function getFormData() {
   var data = {};
   fields.forEach(function(k) {
     data[k] = elements[k].value;
-    if (elements[k].type === 'checkbox') {
-      data[k] = elements[k].checked;
-    }
   });
-  // console.log(data);
   return data;
 }
 
 function handleFormSubmit(event) {  // handles form submit withtout any jquery
+  _eG.canceled = false;
   event.preventDefault();           // we are submitting via xhr below
-  statusMove('email-bar');                    // custom progress bar, not from original code
+  statusMove('email-bar');          // custom progress bar, not from original code
   var data = getFormData();         // get the values submitted in the form
-  if (!validEmail(data.email)) {   // if email is not valid show error
+  if (!validEmail(data.email) || !validCaptcha()) {   // if email is not valid show error
     document.getElementById('email-invalid').style.display = 'block';
+    document.getElementById('email-initial').style.display = 'none';
+    _eG.canceled = true;
     return false;
-  } else {
-    var url = event.target.action;  //
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', url);
-    // xhr.withCredentials = true;
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onreadystatechange = function () {
-        // console.log( xhr.status, xhr.statusText )
-        // console.log(xhr.responseText);
-      document.getElementById('gform').style.display = 'none'; // hide form
-      document.getElementById('thankyou_message').style.display = 'block';
-      return;
-    };
-    // url encode form data for sending as post data
-    var encoded = Object.keys(data).map(function (k) {
-      return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]);
-    }).join('&');
-    xhr.send(encoded);
   }
+  var url = event.target.action;  //
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', url);
+  // xhr.withCredentials = true;
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onreadystatechange = function () {
+      // console.log( xhr.status, xhr.statusText )
+      // console.log(xhr.responseText);
+    document.getElementById('gform').style.display = 'none'; // hide form
+    document.getElementById('thankyou_message').style.display = 'block';
+    return;
+  };
+  // url encode form data for sending as post data
+  var encoded = Object.keys(data).map(function (k) {
+    return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]);
+  }).join('&');
+  xhr.send(encoded);
 }
 function loaded() {
   // console.log('contact form submission handler loaded successfully');
@@ -76,3 +107,4 @@ function loaded() {
   form.addEventListener('submit', handleFormSubmit, false);
 }
 document.addEventListener('DOMContentLoaded', loaded, false);
+captcha();
